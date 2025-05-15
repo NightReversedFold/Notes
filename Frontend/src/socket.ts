959c7cd2baa720fa4439bfd267208c97aba21716
IlogicaAutomatizacion <automatizacion@ilogica-soluciones.cl>
2025-05-15 17:57:30 -0600

    'savedidk'

--- Frontend/src/socket.ts
+++ Frontend/src/socket.ts
@@ -3,7 +3,7 @@ import { io, Socket } from "socket.io-client";
 import type { ServerToClientEvents, ClientToServerEvents } from "./types/webSocket";
 
 const getSocket = (): Socket<ServerToClientEvents, ClientToServerEvents> =>{
-    return io('http://localhost:3000') 
+    return io(process.env.VITE_Api|| 'http://localhost:3000') 
 }
 
 export default getSocket
