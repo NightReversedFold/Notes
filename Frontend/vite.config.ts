b2ce8927cd7ebb81954de0217635022359436050
IlogicaAutomatizacion <automatizacion@ilogica-soluciones.cl>
2025-05-15 18:24:54 -0600

    'savedidk2'

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig,loadEnv } from 'vite'
 
 import react from '@vitejs/plugin-react'
-
-const api = process.env.VITE_Api || 'http://localhost:3000'
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const api = env.VITE_Api || 'http://localhost:3000'
+
+  return {
+  plugins: [react(),tailwindcss()],
   build:{
     outDir: 'dist'
   },
@@ -19,5 +21,5 @@ export default defineConfig({
         ws:true
       }
     },
-  },
+  },}
 })
