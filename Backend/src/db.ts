a09c62dac0e5b78bcafcc77f4c99ffbae24af3f0
IlogicaAutomatizacion <automatizacion@ilogica-soluciones.cl>
2025-05-15 19:59:16 -0600

    'testttxrA'x

--- Backend/src/db.ts
+++ Backend/src/db.ts
@@ -5,7 +5,10 @@ const db = new Pool({
     user: DB_USER,
     host: DB_HOST,
     database: DB_database,
-    password: DB_password
+    password: DB_password,
+    ssl:{
+        rejectUnauthorized:false
+    }
 })
 
 export default db
